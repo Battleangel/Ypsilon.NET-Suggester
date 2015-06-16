4948fc643ed5c4076b4d31ed30b1d1079ba080ca
Daniel Reinhard <battleangle@freenet.de>
2015-06-16 10:53:08 +0200

    add better multi language support

--- js/datepicker.js
+++ js/datepicker.js
@@ -8,11 +8,9 @@
     });
     $("#datepicker1").datepicker({
         defaultDate: "+2d",
-        changeMonth: true,
-        numberOfMonths: 2,
-        showAnim: '',
-        dateFormat: 'dd.mm.yy',
         changeMonth: false,
+        numberOfMonths: 2,
+        showAnim: '',               
         changeYear: false,
         minDate: +2,
         maxDate: "+365d",
@@ -20,14 +18,10 @@
             $("#datepicker2").datepicker("option", "minDate", selectedDate );
             $("#datepicker2").datepicker("show");
             activeDatePicker = 'datepicker2';
-            storeSelDate($(this).datepicker( 'getDate' ),'#Day','#month_1');
-           
+            storeSelDate($(this).datepicker( 'getDate' ),'#Day','#month_1');           
         }
     });
-    var storeSelDate = function(oDate,idDay,idMonthYear) {
-        
-        //console.log(oDate);
-        
+    var storeSelDate = function(oDate,idDay,idMonthYear) {        
         var selDay = oDate.getDate();
         var selMonth = oDate.getMonth()+1;
         var selYear  = oDate.getFullYear();
@@ -37,11 +31,9 @@
     };
     $("#datepicker2" ).datepicker({
         defaultDate: "+1d",
-        changeMonth: true,
-        numberOfMonths: 2,
-        showAnim: '',
-        dateFormat: 'dd.mm.yy',
         changeMonth: false,
+        numberOfMonths: 2,
+        showAnim: '',                
         changeYear: false,
         maxDate: "+365d",
         onClose: function( selectedDate ) {
@@ -49,78 +41,54 @@
             storeSelDate($(this).datepicker( 'getDate' ),'#RetDay','#month_2');
         }
     });
-    
-    var dateToDateObject = function(sDate) {
-        var aDate = sDate.split(".");            
-        return new Date(aDate[2], aDate[1]-1, aDate[0]); 
-    };
-    
+            
     $(document).on('mouseenter', '.ui-datepicker-calendar .ui-state-hover', function(e){
-        //var c1 =        $(this).closest('.ui-datepicker-group');    //closest datepicker...
-        
         
         var day1 =      $(this).text();
-        //var month1 =    c1.find('.ui-datepicker-month').text();
-        //var year1 =     c1.find('.ui-datepicker-year').text();                
+       
+        var oDate1 = $("#datepicker1" ).datepicker('getDate');
+        var oDate2 = $("#datepicker2" ).datepicker('getDate');
         
-        //var fullDate = day1 + "_" + month1 + "_" + year1;
-        // get init date
-        var date1 = $("#datepicker1" ).val();
-        var date2 = $("#datepicker2" ).val();
-        
-        var year1 = $(this).closest( "td" ).data('year');
-        var month1 = $(this).closest( "td" ).data('month');
-        
-        //console.log(year1+'-'+month1+'-'+day1);
+        var year1 = $(this).closest("td").data('year');
+        var month1 = $(this).closest("td").data('month');
         
         var oDateHover = new Date(year1,month1,day1);
         
-        if((activeDatePicker == 'datepicker1') || (date1 === undefined) || (date1 == '')) {
+        if((activeDatePicker == 'datepicker1') || oDate1 === null || (oDate1 === undefined)) {
             return null;
         }
-        var oDate1 = dateToDateObject(date1);
-        var oDate2 = dateToDateObject(date2);
+        
         $('.ui-datepicker-row-break').addClass('ui-datepicker-row-break-info');
-        //$('.ui-datepicker-row-break').html('test');   
-        if((date2 !== undefined) && (date2 != '')) {
+        
+        if((oDate2 !== null) && (oDate2 !== undefined)) {
             var diff = Math.abs((oDate1.valueOf() - oDate2.valueOf())/(24*60*60*1000))+1;
-            $('.ui-datepicker-row-break').html(diff+' Tage'); 
+            $('.ui-datepicker-row-break').html(diff+' '+sTxtDay); 
         }
         
         var diff = Math.abs((oDate1.valueOf() - oDateHover.valueOf())/(24*60*60*1000))+1;
-        $('.ui-datepicker-row-break').html(diff+' Tage'); 
+        $('.ui-datepicker-row-break').html(diff+' '+sTxtDay); 
         
         $('[data-handler=\"selectDay\"]').each(
                 function () {
-                    var cl = $(this).find('.ui-state-default');
-                    
-                    
+                    var cl = $(this).find('.ui-state-default');                                      
                     var year = $(this).data('year');
                     var month = $(this).data('month');
                     var day = cl.text();
 
                     var oItDate = new Date(year, month, day);
-                    //console.log(oDateHover);
-                    //console.log(oItDate);
-                    //console.log(oDate1);
+                    
                     if(!cl.hasClass('ui-state-active')) {
                         if(oItDate.valueOf() == oDate1.valueOf()) {
                             cl.addClass('ui-state-active');
                         }
                     }
-                   
-                    if(oDateHover > oItDate && oDate1 < oItDate) {
-                       // cl.removeClass('ui-state-default');
+                    if(oDateHover > oItDate && oDate1 < oItDate) {                       
                         cl.addClass('ui-state-range');
                     } else {
-                        cl.removeClass('ui-state-range');
-                       // cl.addClass('ui-state-default');
+                        cl.removeClass('ui-state-range');                       
                     }                     
                     
                 }
-        );
-        
-    });
-    
-    
+        );        
+    });        
 });
