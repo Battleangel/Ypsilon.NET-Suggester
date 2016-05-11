0e9ed7eaa4163e418005c74afefbc12d1cfb1416
daniel <dr@yn.de>
2016-05-11 13:36:22 +0200

    removed not needed file

--- suggest.php
+++
@@ -1,4 +0,0 @@
-<?php
-$res = file('http://flweb.ypsilon.net/suggest.php?filterGeoRailway=1&locales='.urlencode($_REQUEST['locales']).'&getGeoByLetter='.urlencode($_REQUEST['getGeoByLetter']).'&letters='.urlencode($_REQUEST['letters']));
-echo utf8_encode(implode("",$res));
-?> 
